dab0338a7b9dccac66d012d100505287eabae9b8
Kike <elviejokike@hotmail.com>
2024-10-19 23:31:13 +0200

    Remove generated types

--- step1/fed-consumer/@mf-types/fed_provider/apis.d.ts
+++
@@ -1,3 +0,0 @@
-
-    export type RemoteKeys = 'fed_provider/button';
-    type PackageType<T> = T extends 'fed_provider/button' ? typeof import('fed_provider/button') :any;
--- step1/fed-consumer/@mf-types/fed_providerfed_provider/apis.d.ts
+++
@@ -1,3 +0,0 @@
-
-    export type RemoteKeys = 'fed_providerfed_provider/button';
-    type PackageType<T> = T extends 'fed_providerfed_provider/button' ? typeof import('fed_providerfed_provider/button') :any;
