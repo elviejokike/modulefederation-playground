f51229e9c62437a5b90021aa4e1a2a2eab03c3cc
Kike <elviejokike@hotmail.com>
2024-10-20 11:32:09 +0200

    Update layout to make it readable

--- federation-runtime/fed-consumer/src/App.tsx
+++ federation-runtime/fed-consumer/src/App.tsx
@@ -1,13 +1,32 @@
+import { useState } from 'react';
 import './App.css';
 import { DynamicView } from './viewService';
 
 const App = () => {
+  const [comp, setComponent] = useState("fed_provider/Comp1");
+  const [view, setView] = useState(comp);
+  const handleChange = (e:any) => {
+    setComponent(e.target.value);
+  };
   return (
-    <div className="content">
-      <h1>Dynamic View</h1>
-      <p>Embed remote views with Modoule Federation Runtime</p>
-      <DynamicView module="fed_provider/Comp2" color="green" fontSize="28px"/>
-    </div>
+    <>
+      <div className="content">
+        <h1>Dynamic View</h1>
+        <p>Embed remote views with Modoule Federation Runtime</p>
+      </div>
+      <div className="component">
+        <input
+          type="text"
+          name="surName"
+          value={comp}
+          onChange={handleChange}
+        />
+        <button type="button" onClick={() => setView(comp)}>Set Component</button>
+      </div>
+      <div className="view">
+        <DynamicView module={view} color="green" fontSize="28px"/>
+      </div>
+    </>
   );
 };
 
